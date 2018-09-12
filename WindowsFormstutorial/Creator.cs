3b6fffc187fb7448c67136b94a06a18584c94779
Mondotrasho <oscar@peoplepictures.com.au>
2018-09-12 20:55:14 +1000

    Add project files.

---
+++ WindowsFormstutorial/Creator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormstutorial
+{
+    public class Creator
+    {
+        public List<int[]> Box;
+
+        public void Add(int a , int b)
+        {
+           Box.Add(new []{a,b});
+        }
+        public void DrawPoint(PictureBox picture)
+        {
+        
+            foreach (var bit in Box)
+            {
+                Color color = Color.DarkRed;
+                Graphics g = picture.CreateGraphics();
+                
+                Pen pen = new Pen(color);
+                SolidBrush fill = new SolidBrush(color);
+                // g.DrawRectangle(pen,new Rectangle(bit[0], bit[1],5,5));//DrawLine(pen, new Point(bit[0], bit[0]), new Point(x, y));
+                g.DrawRectangle(pen, new Rectangle(bit[0], bit[1] - 6, 1, 13));
+                g.DrawRectangle(pen, new Rectangle(bit[0] - 6, bit[1] , 13, 1));
+                g.FillEllipse(fill ,new Rectangle(bit[0]-2, bit[1]-2, 5, 5));
+            }
+        }
+    }
+}
---
+++ WindowsFormstutorial/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormstutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 304);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(0, 310);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(39, 128);
+            this.buttonExport.TabIndex = 2;
+            this.buttonExport.Text = "E\r\nX\r\nP\r\nO\r\nR\r\nT";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(45, 310);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(708, 128);
+            this.textOutput.TabIndex = 3;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(759, 310);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(41, 128);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "C\r\nL\r\nE\r\nA\r\nR";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
